62425162c08544d65384f72324ccdc5c30f00301
pyprism <areose21@gmail.com>
2014-01-15 22:02:27 +0600

    folder restructure :/

--- angularjs/dist/js/controler.js
+++ angularjs/dist/js/controler.js
@@ -3,17 +3,29 @@ var hirenx = angular.module('hiren',['ngRoute']);
 hirenx.config(['$routeProvider',
 	function($routeProvider){
 		$routeProvider
+		.when("/" ,{
+			templateUrl:'home.html'
+			//controller: 'hirenx'
+		})
 		.when("/explore" ,{
-			templateUrl:'alphaselect.html',
+			templateUrl:'parials/alphaselect.html',
 			controller: 'hirenx'
 		})
-		.when('/x' ,{
-			templateUrl: 'artistsName.html' ,
+		.when('/artist/:name' ,{
+			templateUrl: 'parials/artistsName.html' ,
 			controller : 'hireny'
 		})
-		.when('/hiren' , {
-			templateUrl : 'download.html',
+		.when('/artist/:name/album/:albumname' , {
+			templateUrl : 'parials/download.html',
 			controller : 'hirenz'
+		})
+		.when('/about' , {
+			templateUrl : 'parials/about.html'
+			//controller : 'hirenz'
+		})
+		.when('/playlist' , {
+			templateUrl : 'parials/playlist.html'
+			//controller : 'hirenz'
 		})
 		.otherwise({
 			redirectTo: '/'
@@ -26,15 +38,13 @@ hirenx.config(['$routeProvider',
 var rootURL = "http://localhost/music/public/index.php/";
 
 hirenx.controller('hireny' , function($scope ,$http , $location){
-	//$http.defaults.useXDomain = true;
 	$http.get( rootURL + 'alpha').success(function(data){
 		$scope.message = data ;
 		$scope.click = function(value){
-			//console.log(value);
 			var x = {'name' : value};
 			$http.post( rootURL + 'alphapost', x).success(function(data){
 				console.log('data send');
-				$location.path(value);
+				$location.path("/hiren");
 			});
 		}
 	});
