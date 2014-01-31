60bd0a394a35c5e302ca6e5fb2497ca4c9733b41
pyprism <areose21@gmail.com>
2014-01-31 21:39:44 +0600

    take care of Others music files

--- app/scraper/index.php
+++ app/scraper/index.php
@@ -47,8 +47,6 @@ function naima(){
      return $nisha;
 }
 
-var_dump(naima());
-
 
 
 function get_album_list($alpha , $artist_name){
@@ -71,6 +69,42 @@ function get_album_list($alpha , $artist_name){
      return $album_name ;
 }
 
+
+function get_album_list_copy($alpha , $artist_name){
+    $html = file_get_html('http://www.music.com.bd/download/browse' . '/' . $alpha . '/' . rawurlencode($artist_name) . "/");
+foreach ($html->find('a.autoindex_a') as $link) {
+    foreach($link->find('strong') as $tag){
+        $name = $tag->plaintext ;
+        $raw_album_name[] = $name ;
+        }
+    }
+
+    foreach ($html->find('a[class=autoindex_a snap_shots]') as $link) {
+        if(isset($link)){
+            foreach($link->find('strong') as $tag)
+            {
+                $name = $tag->plaintext ;
+                $mp3[] = $name ;
+            }
+        }
+    }
+    if(isset($mp3)){
+        $sorted_album = array_diff($raw_album_name, $mp3);
+        array_shift($sorted_album); // removes "parent directory"
+        array_push($sorted_album, 'Others');
+        return $sorted_album ;
+
+    }
+    else{
+        array_shift($raw_album_name);
+        return $raw_album_name;
+    }
+
+}
+
+
+var_dump(get_album_list_copy('A','Adhar'));
+
 //http://stackoverflow.com/questions/834303/php-startswith-and-endswith-functions/834355#834355
 function endsWith($haystack, $needle)
 {
@@ -79,28 +113,54 @@ function endsWith($haystack, $needle)
 
 function get_music_list($alpha , $name , $album){
   $song_collection = [] ;
-  $html = file_get_html('http://www.music.com.bd/download/browse' . "/" . $alpha . '/' . rawurlencode($name) . "/" . rawurlencode($album) . "/");
-  foreach ($html->find('.snap_shots') as $link) {
-      $al =$link->href;
-  foreach($link->find('strong') as $tag)
-       {
-             $name = $tag->plaintext ;
-            $final_name = str_replace("(music.com.bd).mp3", "", $name);
-            if(!(endsWith($al , "zip.html")))   //Removes zip download
-                 $x = preg_replace("/ /", '%20', $al);
-            else
-              break;
-             $html = file_get_html( $x );
-             foreach ($html->find('#download_link a') as $link) {
-                $linku =$link->href;
-               // $song[$final_name] = $linku ;
-                $song = [ 'name' => $final_name , 'url' => $linku];
-                array_push($song_collection , $song) ;
-             }
-             //
-       }
-     }
-     return $song_collection;
+  if($album != "Others"){
+      $html = file_get_html('http://www.music.com.bd/download/browse' . "/" . $alpha . '/' . rawurlencode($name) . "/" . rawurlencode($album) . "/");
+      foreach ($html->find('.snap_shots') as $link) {
+          $al =$link->href;
+          foreach($link->find('strong') as $tag)
+          {
+              $name = $tag->plaintext ;
+              $final_name = str_replace("(music.com.bd).mp3", "", $name);
+              if(!(endsWith($al , "zip.html")))   //Removes zip download
+                  $x = preg_replace("/ /", '%20', $al);
+              else
+                  break;
+              $html = file_get_html( $x );
+              foreach ($html->find('#download_link a') as $link) {
+                  $linku =$link->href;
+                  // $song[$final_name] = $linku ;
+                  $song = [ 'name' => $final_name , 'url' => $linku];
+                  array_push($song_collection , $song) ;
+              }
+              //
+          }
+      }
+      return $song_collection;
+  }
+  else{
+      $html = file_get_html('http://www.music.com.bd/download/browse' . "/" . $alpha . '/' . rawurlencode($name) . "/" );
+      foreach ($html->find('a[class=autoindex_a snap_shots]') as $link) {
+          if(isset($link)){
+              foreach($link->find('strong') as $tag)
+              {
+                  $name = $tag->plaintext ;
+                  $mp3[] = $name ;
+              }
+          }
+      }
+
+      $html = file_get_html( $x );
+      foreach ($html->find('#download_link a') as $link) {
+          $linku =$link->href;
+          // $song[$final_name] = $linku ;
+          $song = [ 'name' => $final_name , 'url' => $linku];
+          array_push($song_collection , $song) ;
+      }
+
+     return $song_collection ;
+
+  }
+
 }
 //Todo : link availablity checker
 // $songs[$name]=$url;
