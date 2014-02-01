3c5e0d763d06d9f5ca8f692548b796c519a9f2c3
pyprism <areose21@gmail.com>
2014-02-01 20:54:58 +0600

    removed backup function

--- app/scraper/index.php
+++ app/scraper/index.php
@@ -32,43 +32,6 @@ function select_artist_by_name($alpha){
 
 }
 
-//A.autoindex_a.snap_shots
-function naima(){
-  //$html = file_get_html('http://www.music.com.bd/download/browse/A/Aashor/');
-  $html = file_get_html('http://www.music.com.bd/download/browse/A/Arbovirus/');
-  foreach ($html->find('a[class=autoindex_a snap_shots]') as $link) {
-    if(isset($link)){
-   foreach($link->find('strong') as $tag)
-       {
-             $name = $tag->plaintext ;
-             $nisha[] = $name ;
-       }
-     }
-   }
-     return $nisha;
-}
-
-
-
-function get_album_list_bak($alpha , $artist_name){
-
-  $html = file_get_html('http://www.music.com.bd/download/browse' . '/' . $alpha . '/' . rawurlencode($artist_name) . "/") ;
-    foreach ($html->find('a.autoindex_a') as $link) {
-    foreach($link->find('strong') as $tag)
-       {
-             $name = $tag->plaintext ;
-             $album_name[] = $name ;
-       }
-     }
-     //To do - lonely mp3 file check
-    foreach ($html->find('a.snap_shots') as $link) {
-      if(isset($link)){
-        $album_name[] = 'others';
-     }
-   }
-     array_shift($album_name); //Removed "parent directory"
-     return $album_name ;
-}
 
 
 function get_album_list($alpha , $artist_name){
@@ -158,40 +121,6 @@ function get_music_list($alpha , $name , $album){
 
   }
 
-}
-
-
-function nisha($alpha, $name){
-      $song_collection = [] ;
-     $html = file_get_html('http://www.music.com.bd/download/browse' . "/" . $alpha . '/' . rawurlencode($name) . "/" );
-      foreach ($html->find('a[class=autoindex_a snap_shots]') as $link) {
-          if(isset($link)){
-              foreach($link->find('strong') as $tag)
-              {
-                  /**$name = $tag->plaintext ;
-              $final_name = str_replace("(music.com.bd).mp3", "", $name);
-              if(!(endsWith($al , "zip.html")))   //Removes zip download
-                  $x = preg_replace("/ /", '%20', $al);
-              else
-                  break;
-              $html = file_get_html( $x );
-              foreach ($html->find('#download_link a') as $link) {
-                  $linku =$link->href;
-                  // $song[$final_name] = $linku ;
-                  $song = [ 'name' => $final_name , 'url' => $linku];
-                  array_push($song_collection , $song) ;
-                }**/
-                $name = $tag->plaintext;
-                $link = $link->href;
-                $song = ['name'=> $name , 'url'=> $link];
-                array_push($song_collection, $song);
-              }
-          }
-        }
-
-
-
-     return $song_collection ;
 }
 
 
