5575f0d046fbd5db66d56a1d18ab82b6ddee4432
Ashutosh Das <git.pyprism@gmail.com>
2015-12-19 22:25:29 +0600

    add new views

--- music/views.py
+++ music/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Music, Dropbox, Album
+from .serializers import AlbumSerializer, DropboxSerializer, MusicSerializer
 
-# Create your views here.
+
+class AlbumList(generics.ListCreateAPIView):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+
+class AlbumDetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Create, retrieve, update or delete a instance
+    """
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+
+class MusicDetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Create, retrieve, update or delete a instance
+    """
+    model = Music
+    serializer_class = MusicSerializer
+
+
+class DropboxDetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Create, retrieve, update or delete a instance
+    """
+    model = Dropbox
+    serializer_class = DropboxSerializer
