fa579f6828e1ca25ba51a6e80b284630d2791310
Ashutosh Das <git.pyprism@gmail.com>
2015-12-28 21:49:05 +0600

    add gwt

--- hiren/urls.py
+++ hiren/urls.py
@@ -5,7 +5,8 @@ from music import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.AlbumList.as_view()),
-                       url(r'^(?P<pk>[0-9]+)/$', views.AlbumDetails.as_view())
+                       url(r'^(?P<pk>[0-9]+)/$', views.AlbumDetails.as_view()),
+                       url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
 
 )
 
