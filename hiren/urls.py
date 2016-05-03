12baec36198afd1eb9c2ba1c4bdc728240b9b3f5
Ashutosh Das <git.pyprism@gmail.com>
2016-05-03 15:05:12 +0600

    updated url

--- hiren/urls.py
+++ hiren/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from music import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-                       url(r'^$', views.AlbumList.as_view()),
-                       url(r'^(?P<pk>[0-9]+)/$', views.AlbumDetails.as_view()),
+                       url(r'^$', TemplateView.as_view(template_name="login.html"), name='whatever'),
+                       url(r'^albumList$', views.AlbumList.as_view()),
+                       url(r'^albumList/(?P<pk>[0-9]+)/$', views.AlbumDetails.as_view()),
                        url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
                        url(r'^docs/', include('rest_framework_swagger.urls')),
 
