f385de1bc705b58fe9dfa562e83d998817c2d053
Ashutosh Das <git.pyprism@gmail.com>
2016-06-22 21:56:25 +0600

    Exploring :smile:

--- music/views.py
+++ music/views.py
@@ -4,7 +4,42 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .models import Album, Music, Playlist
+from .serializers import AlbumSerializer, PlaylistSerializer, MusicSerializer
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework import status
+from hiren.settings import JSON_DATA
+from braces.views import CsrfExemptMixin
 import dropbox
+
+
+class AlbumViewSet(CsrfExemptMixin, viewsets.ModelViewSet):
+    """
+    API endpoint that allows album to be created, viewed ,edited and deleted.
+    """
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+
+class PlaylistViewSet(CsrfExemptMixin, viewsets.ModelViewSet):
+    """
+    API endpoint that allows playlist to be created, viewed ,edited and deleted.
+    """
+    queryset = Playlist.objects.all().order_by('-created_at')
+    serializer_class = PlaylistSerializer
+
+
+class MusicViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows music files and related info to be created, viewed ,edited and deleted.
+    """
+    queryset = Music.objects.all()
+    serializer_class = MusicSerializer
+
+    def perform_create(self, serializer):
+        file_obj = self.request.data['file']
+        print('hot')
+        dbx = dropbox.Dropbox(JSON_DATA['dropbox_access_token'])
+        res = dbx.files_upload(file_obj, '/', autorename=True, mute=True)
+        print(res)
+        #serializer.save(dropbox_id='x')
