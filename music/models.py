ab6e0c7dc884af83851b5749c3c81ae8cd9771bd
Ashutosh Das <git.pyprism@gmail.com>
2016-06-22 11:44:48 +0600

    updated models

--- music/models.py
+++ music/models.py
@@ -1,35 +1,24 @@
 from django.db import models
-from django.template.defaultfilters import slugify
-import datetime
 
 
 class Album(models.Model):
     name = models.CharField(max_length=100, unique=True)
     favorite = models.BooleanField(default=False)
-    added_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    created_at = models.DateField(auto_now_add=True)
 
 
 class Music(models.Model):
     album = models.ForeignKey(Album)
+    playlist = models.ForeignKey(Playlist, null=True)
     name = models.CharField(max_length=200, unique=True)
-    slug = models.CharField(max_length=150, unique=True)
+    dropbox_id = models.CharField(max_length=500, null=True)
     favorite = models.BooleanField(default=False)
-    added_at = models.DateField(auto_now_add=True)
-    location = models.URLField(null=True, default=None)
-    local_location = models.FileField(upload_to='hiren/%Y/%m/%d', null=True, default=None)
-
-    def save(self):
-        super().save()
-        date = datetime.date.today()
-        self.slug = '%i/%i/%i/%s' % (
-            date.year, date.month, date.day, slugify(self.name)
-        )
-        super().save()
+    created_at = models.DateField(auto_now_add=True)
+    counter = models.IntegerField(default=0)
 
 
-class Dropbox(models.Model):
-    enabled = models.BooleanField(default=False)
-    access_token = models.CharField(max_length=300)
-    client_id = models.CharField(max_length=300)
-    client_secret = models.CharField(max_length=300)
-    redirect_uri = models.URLField()
