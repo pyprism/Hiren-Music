e61e7e24c1a909ace4f24101440857045f46e13e
Ashutosh Das <git.pyprism@gmail.com>
2016-06-22 11:44:25 +0600

    fixed import

--- music/serializers.py
+++ music/serializers.py
@@ -1,23 +1,20 @@
 from rest_framework import serializers
-from .models import Album, Dropbox, Music
+from .models import Album, Music, Playlist
 
 
 class AlbumSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Album
-        fields = ('id', 'name')
 
 
 class MusicSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Music
-        fields = ('id', 'album', 'name', 'favorite')
 
 
-class DropboxSerializer(serializers.ModelSerializer):
+class PlaylistSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Dropbox
-        fields = '__all__'
+        model = Playlist
