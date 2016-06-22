b6ab98b4fcb807ee9dbdc63c60eca4721d58a688
Ashutosh Das <git.pyprism@gmail.com>
2016-06-22 11:44:18 +0600

    fixed import

--- music/views.py
+++ music/views.py
@@ -3,8 +3,8 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from .models import Tag, Diary, Notes, Secret
+from .models import Album, Music, Playlist
 from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework import status
-
+import dropbox
