3f4b72cb923356b5cf636e33ad717f9858d901c0
Ashutosh Das <git.pyprism@gmail.com>
2016-06-22 21:56:06 +0600

    removed slash

--- hiren/urls.py
+++ hiren/urls.py
@@ -5,12 +5,15 @@ from rest_framework import routers
 from music import views
 
 router = routers.DefaultRouter()
+router.register('album', views.AlbumViewSet)
+router.register('playlist', views.PlaylistViewSet)
+router.register('music', views.MusicViewSet)
 
 urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include('rest_framework_docs.urls')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
