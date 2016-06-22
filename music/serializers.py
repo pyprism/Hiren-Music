732b274558d5f28184b1bd65066f46b5f314e2c7
Ashutosh Das <git.pyprism@gmail.com>
2016-06-22 21:56:34 +0600

    Exploring :smile:

--- music/serializers.py
+++ music/serializers.py
@@ -3,18 +3,18 @@ from .models import Album, Music, Playlist
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Album
 
 
 class MusicSerializer(serializers.ModelSerializer):
+    file = serializers.FileField(required=True)
 
     class Meta:
         model = Music
+        fields = ('id', 'favorite', 'created_at', 'counter', 'file', 'name', 'album', 'playlist')
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Playlist
