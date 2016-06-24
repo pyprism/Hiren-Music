1625b5cda01134cfe0ee382d3ec3965db4023453
Ashutosh Das <git.pyprism@gmail.com>
2016-06-24 23:31:36 +0600

    Added new module

--- static/apps/app.jsx
+++ static/apps/app.jsx
@@ -1,3 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import Login from './components/Login.jsx';
+import Main from './components/Main.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import Upload from './components/Upload.jsx';
+import axios from 'axios';
+
+function authRequired(nextState, replace) {
+    let token = sessionStorage.getItem('token');
+    if (token) {
+        axios({
+            method: 'post',
+            url: '/api-token-verify/',
+            data: {
+                'token': token
+            }
+        }).then(function (res) {
+            console.log(res);
+        }).catch(function(response) {
+            replace('/');
+            console.log(response);
+            sweetAlert("Oops!", 'Token Expired', "info");
+        });
+    } else {
+        replace('/');
+    }
+
+}
+
+ReactDOM.render(
+    <Router history={browserHistory} >
+        <Route path="/" component={Login} />
+        <Route path="/dashboard" onEnter={authRequired} component={Main}>
+            <IndexRoute  component={Dashboard}/>
+            <Route path="stats"  component={Dashboard} />
+            <Route path="upload"  component={Upload} />
+        </Route>
+    </Router>,
+    document.getElementById('app')
+);
