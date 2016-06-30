f2aba974bf5845ef14c7951ba27fcf9d2e9bfedb
Ashutosh Das <git.pyprism@gmail.com>
2016-06-30 20:57:58 +0600

    added temporary component

--- static/apps/components/Upload.jsx
+++ static/apps/components/Upload.jsx
@@ -1,51 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Helmet from "react-helmet";
-import axios from 'axios';
 
 
 export default class Upload extends React.Component {
 
     upload(e) {
         e.preventDefault();
-        axios({
-            method: 'post',
+        let data = new FormData();
+        data.append('name', ReactDOM.findDOMNode(this.refs.name).value);
+        data.append('album', ReactDOM.findDOMNode(this.refs.album).value);
+        data.append('playlist', 1);
+        data.append('file', ReactDOM.findDOMNode(this.refs.file).files[0]);
+        $.ajax({
             url: '/api/music/',
+            method: 'post',
             headers: {
-                'Authorization': 'JWT ' + sessionStorage.getItem('token'),
-                'Content-Type': 'multipart/form-data'
+                'Authorization': 'JWT ' + sessionStorage.getItem('token')
             },
-            data: {
-                'name': ReactDOM.findDOMNode(this.refs.name).value,
-                'album': ReactDOM.findDOMNode(this.refs.album).value,
-                'files': ReactDOM.findDOMNode(this.refs.file).value
-            }
-        }).then(function (response) {
-            console.log(response);
-        }).catch(function (response) {
-            console.log(response);
+            contentType: false,
+            data: data,
+            processData: false
+        }).done(function (res) {
+            $.notify("Music Saved", "success");
+        }).fail(function (error) {
+            if (error.responseText)
+                sweetAlert("Oops!", error.responseText, "error");
         });
     }
 
 
-render() {
-    return (
-        <div>
-            <form onSubmit={this.upload.bind(this)}>
-                <div className="form-group">
-                    <label>Title</label>
-                    <input type="text" className="form-control" required ref="name" placeholder="Title" />
-                </div>
-                <div className="form-group">
-                    <label>Album</label>
-                    <input type="text" className="form-control" ref="album" placeholder="Album Name"/>
-                </div>
-                <div className="form-group btn btn-default btn-file">
-                    <input type="file"  ref="file" />
-                </div>
-                <button type="submit" className="btn btn-default"><i className="fa fa-bookmark"> Save</i></button>
-            </form>
-        </div>
-    )
-}
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.upload.bind(this)}>
+                    <div className="form-group">
+                        <label>Title</label>
+                        <input type="text" className="form-control" required ref="name" placeholder="Title" />
+                    </div>
+                    <div className="form-group">
+                        <label>Album</label>
+                        <input type="text" className="form-control" ref="album" placeholder="Album Name"/>
+                    </div>
+                    <div className="form-group btn btn-default btn-file">
+                        <input type="file"  ref="file" />
+                    </div>
+                    <button type="submit" className="btn btn-default"><i className="fa fa-bookmark"> Save</i></button>
+                </form>
+            </div>
+        )
+    }
 }
