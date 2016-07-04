35accb5ca5a799a4530698b969127ebee7b778ae
Ashutosh Das <git.pyprism@gmail.com>
2016-07-04 23:53:55 +0600

    fixed dropbox location

--- music/models.py
+++ music/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Album(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    cover_art = models.CharField(max_length=500, null=True)
+    location = models.CharField(max_length=500, null=True)
     has_cover = models.BooleanField(default=False)
     favorite = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
@@ -19,7 +19,7 @@ class Music(models.Model):
     album = models.ForeignKey(Album, related_name='tracks')
     playlist = models.ForeignKey(Playlist, null=True, related_name='tracks')
     name = models.CharField(max_length=200, unique=True)
-    dropbox_id = models.CharField(max_length=500)
+    location = models.CharField(max_length=500)
     favorite = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
     counter = models.IntegerField(default=0)
