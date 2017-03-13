3b2f3989e3231f943552c88af19c3a3b4f2da329
Ashutosh Das <git.pyprism@gmail.com>
2017-03-13 21:01:14 +0600

    wip

--- hiren/urls.py
+++ hiren/urls.py
@@ -2,18 +2,18 @@ from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 from rest_framework import routers
-from music import views
-
+# from music import views
+#
 router = routers.DefaultRouter()
-router.register('album', views.AlbumViewSet)
-router.register('playlist', views.PlaylistViewSet)
-router.register('music', views.MusicViewSet)
-
+# router.register('album', views.AlbumViewSet)
+# router.register('playlist', views.PlaylistViewSet)
+# router.register('music', views.MusicViewSet)
+#
 urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^docs/', include('rest_framework_swagger.urls')),
+#     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
