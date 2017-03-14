bf18a6fb865b50d8b33d136ef067735515dbea38
Ashutosh Das <git.pyprism@gmail.com>
2017-03-14 20:14:38 +0600

    added new route

--- hiren/urls.py
+++ hiren/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework import routers
 # from music import views
 #
@@ -12,6 +13,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 #     url(r'^docs/', include('rest_framework_swagger.urls')),
