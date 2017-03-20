138408bfc5cee18faadfa924b79b683a6224d88d
Ashutosh Das <git.pyprism@gmail.com>
2017-03-20 17:39:53 +0600

    changed file name

---
+++ bunny/app.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Login from './components/Login';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+
+
+function refresh() {
+    let token = localStorage.getItem('token');
+    if(token){
+        axios({
+            method: 'post',
+            url: '/api-token-refresh/',
+            data: {
+                'token': token
+            }
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.error(error);
+            sweetAlert("Oops!", error.statusText, "error");
+        });
+    }
+}
+
+setInterval(refresh(), 60 * 60);
+
+function authRequired(nextState, replace) {
+    let token = localStorage.getItem('token');
+    if (token) {
+        axios({
+            method: 'post',
+            url: '/api-token-verify/',
+            data: {
+                'token': token
+            }
+        }).then(function (res) {
+
+        }).catch(function(response) {
+            replace('/');
+            sweetAlert("Oops!", 'Token Expired', "error");
+        });
+    } else {
+        replace('/');
+    }
+
+}
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+// ReactDOM.render(<HelloMessage name="Naima" />, document.getElementById('app'));
+
+ReactDOM.render(
+    <Router history={browserHistory}>
+        <Route path="/" name="Naima" component={Login}/>
+    </Router>,
+    document.getElementById('app')
+);
+
