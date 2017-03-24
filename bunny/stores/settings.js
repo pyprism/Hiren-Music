dba80189f7dfa1016a266e73bb50337006fb3ed1
Ashutosh Das <git.pyprism@gmail.com>
2017-03-24 13:29:59 +0600

    added new store

---
+++ bunny/stores/settings.js
@@ -0,0 +1,30 @@
+/**
+ * Created by prism on 3/24/17.
+ */
+import { observable, action, computed, autorun } from 'mobx';
+import axios from 'axios';
+import { toJS } from "mobx";
+
+export class settings {
+    @observable loaded = false;
+    @observable conf = false;
+    @observable values = [];
+    @observable loadingText = 'Loading from remote server....';
+
+    @action getData() {
+        this.values = [];
+        axios({
+            method: 'get',
+            url: '/api/bucket/',
+            headers: {'Authorization': "JWT " + localStorage.getItem('token')}
+        }).then(function (response) {
+            if((response.data).length) {
+                this.conf = true;
+            }
+            this.loaded = true;
+        }.bind(this)).catch(function (err) {
+            console.error(err);
+            sweetAlert("Oops!", err.statusText, "error");
+        })
+    }
+}
