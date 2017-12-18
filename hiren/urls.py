b1cd73ef7268fad44ac955758ff34bf94ae81b6a
Ashutosh Das <git.pyprism@gmail.com>
2017-12-18 21:12:41 +0600

    removed url

--- hiren/urls.py
+++ hiren/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
-from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
-from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework import routers
 from music import views
 
@@ -12,9 +10,6 @@ router.register('bucket', views.B2AccountViewSet)
 # router.register('music', views.MusicViewSet)
 #
 urlpatterns = [
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 #     url(r'^docs/', include('rest_framework_swagger.urls')),
