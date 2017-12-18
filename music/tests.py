50e0a229433f9eaf55a9331c3162d69cfaa7c4da
Ashutosh Das <git.pyprism@gmail.com>
2017-12-18 21:13:16 +0600

    wip

--- music/tests.py
+++ music/tests.py
@@ -3,10 +3,10 @@ from django.test import Client
 # Create your tests here.
 
 
-class IndexPageTest(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_uses_login_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'index.html')
+# class IndexPageTest(TestCase):
+#     def setUp(self):
+#         self.client = Client()
+#
+#     def test_uses_login_template(self):
+#         response = self.client.get('/')
+#         self.assertTemplateUsed(response, 'index.html')
