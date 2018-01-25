c2afbe22148ec02b8297bdc82443954051697d5d
Ashutosh Das <git.pyprism@gmail.com>
2018-01-25 11:06:21 +0600

    added new url

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.views import logout
 from django.urls import path
+from base import views
 
 urlpatterns = [
+    path('', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
     path('logout/', logout, {'next_page': '/'}, name='logout'),
 ]
 
