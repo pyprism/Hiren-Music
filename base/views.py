7af3668bfa0370554bfaf8246aa2f11a416133f3
Ashutosh Das <git.pyprism@gmail.com>
2018-09-04 19:23:27 +0600

    added login view

--- base/views.py
+++ base/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from rest_framework import status
+from django.contrib import auth
 
-# Create your views here.
+
+def login(request):
+    """
+    token authentication for api
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            token = Token.objects.get_or_create(user=user)
+            return JsonResponse({'token': str(token[0])}, status=status.HTTP_200_OK)
+        return JsonResponse({'error': 'Username/Password is not valid'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
