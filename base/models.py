38046cd573111e3fb334ec86b32daa6c2f48c16c
Ashutosh Das <git.pyprism@gmail.com>
2018-09-04 13:14:49 +0600

    added custom user model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+
+    def create_user(self, username=None, password=None, **kwargs):
+        account = self.model(username=username)
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, username, password, **kwargs):
+        account = self.create_user(username, password, **kwargs)
+        account.is_admin = True
+        account.save()
+        return account
+
+
+class Account(AbstractBaseUser):
+    username = models.CharField(max_length=40, unique=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'username'
+
+    objects = AccountManager()
+
+
+class Setting(models.Model):
+    active = models.BooleanField(default=True)
+    task_type = (
+        ('S', 'Signup'),
+    )
+    task = models.CharField(choices=task_type, max_length=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
