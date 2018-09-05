fdee5c16ce984f97999200de68536cf41abfe4a4
Ashutosh Das <git.pyprism@gmail.com>
2018-09-05 21:23:02 +0600

    changed form

--- bunny/src/components/pages/Register.js
+++ bunny/src/components/pages/Register.js
@@ -1,30 +1,74 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
 import '../../css/auth.css';
 
 
 export default class Register extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            repeat_password: ""
+        }
+    }
+
+    componentDidMount() {
+        document.title = "Hiren-Music: Register";
+    }
+
+    handleUsernameChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value});
+    }
+
+    handleRepeatPasswordChange(event) {
+        this.setState({repeat_password: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        if((this.state.password).length <=0 || (this.state.repeat_password).length <= 0) {
+            return;
+        }
+        if((this.state.username).length <=0 ) {
+            return;
+        }
+        if(this.state.password !== this.state.repeat_password) {
+            return;
+        }
+        console.log("ok");
+    }
+
     render() {
+
+        const {username, password, repeat_password} = this.state;
+
         return (
             <div className="lowin lowin-blue">
                 <div className="lowin-brand">
                     <img src={require('../../image/drum.svg')} alt="logo"/>
                 </div>
                 <div className="lowin-wrapper">
-                <div className="lowin-box lowin-register">
+                    <div className="lowin-box lowin-register">
                         <div className="lowin-box-inner">
-                            <form>
+                            <form onSubmit={this.handleSubmit.bind(this)}>
                                 <p>Let's create your account</p>
                                 <div className="lowin-group">
-                                    <label>Name</label>
-                                    <input type="text" name="name" autoComplete="name" className="lowin-input"/>
-                                </div>
-                                <div className="lowin-group">
-                                    <label>Email</label>
-                                    <input type="email" autoComplete="email" name="email" className="lowin-input"/>
+                                    <label>Username</label>
+                                    <input type="text" value={username} autoFocus onChange={this.handleUsernameChange.bind(this)} className="lowin-input"/>
                                 </div>
                                 <div className="lowin-group">
                                     <label>Password</label>
-                                    <input type="password" name="password" autoComplete="current-password"
+                                    <input type="password" value={password} onChange={this.handlePasswordChange.bind(this)} className="lowin-input"/>
+                                </div>
+                                <div className="lowin-group">
+                                    <label>Repeat Password</label>
+                                    <input type="password" value={repeat_password} onChange={this.handleRepeatPasswordChange.bind(this)}
                                            className="lowin-input"/>
                                 </div>
                                 <button className="lowin-btn">
@@ -32,7 +76,7 @@ export default class Register extends Component {
                                 </button>
 
                                 <div className="text-foot">
-                                    Already have an account? <a href="" className="login-link">Login</a>
+                                    Already have an account? <Link to="/" className="login-link">Login</Link>
                                 </div>
                             </form>
                         </div>
