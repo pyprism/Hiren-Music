e5b3b566355c82887f41b77247aa8fb414442887
Ashutosh Das <git.pyprism@gmail.com>
2018-09-06 10:10:04 +0600

    added base url

--- hiren/urls.py
+++ hiren/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
+from base import urls as base
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('api/base/', include(base)),
 ]
