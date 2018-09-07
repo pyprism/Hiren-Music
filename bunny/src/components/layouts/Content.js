ff724d772ed8cac122fe8a8469900914502cdcef
Ashutosh Das <git.pyprism@gmail.com>
2018-09-07 21:21:36 +0600

    Added new component

---
+++ bunny/src/components/layouts/Content.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+import '../../css/dashboard.css';
+
+export default class Content extends React.Component {
+
+    componentDidMount() {
+        const $ = window.$;
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <Sidebar/>
+                <div id="content">
+                    <Navbar/>
+                    bunny
+                </div>
+            </div>
+        )
+    }
+}
