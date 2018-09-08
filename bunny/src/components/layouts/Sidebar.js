5ed3ec03225192c9158dae13e4146958dcec45e3
Ashutosh Das <git.pyprism@gmail.com>
2018-09-08 16:30:02 +0600

    wip

--- bunny/src/components/layouts/Sidebar.js
+++ bunny/src/components/layouts/Sidebar.js
@@ -1,37 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 export default function Sidebar() {
     return (
         <nav id="sidebar">
             <div className="sidebar-header">
                     <h3> <img src={require('../../image/drum.svg')} width="30" height="30" alt="logo"/> Hiren Music</h3>
-                    <strong> <img src={require('../../image/drum.svg')} width="30" height="30" alt="logo"/></strong>
+                    <strong> <img src={require('../../image/drum.svg')} width="40" height="50" alt="logo"/></strong>
             </div>
 
             <ul className="list-unstyled components">
                 <li className="active">
-                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">
-                        <i className="fas fa-home"/>
-                        Home
-                    </a>
-                    <ul className="collapse list-unstyled" id="homeSubmenu">
-                        <li>
-                            <a href="#">Home 1</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 2</a>
-                        </li>
-                        <li>
-                            <a href="#">Home 3</a>
-                        </li>
-                    </ul>
+                    <NavLink to="/music" activeClassName="active" >
+                        <FontAwesomeIcon icon={faHome} /> Home
+                    </NavLink>
                 </li>
                 <li>
-                    <a href="#">
+                    <NavLink to="#" activeClassName="active">
                         <i className="fas fa-briefcase"/>
                         About
-                    </a>
+                    </NavLink>
                     <a href="#pageSubmenu">
                         <i className="fas fa-copy"/>
                         Pages
