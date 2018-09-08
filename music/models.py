2b5dbc8201a5bd4a37796e4f18f5f69e27ed9fcd
Ashutosh Das <git.pyprism@gmail.com>
2018-09-08 17:04:15 +0600

    added model

--- music/models.py
+++ music/models.py
@@ -20,9 +20,52 @@ class Album(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class Track(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, related_name='tracks', null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, unique=True)
+    artist = models.CharField(max_length=200, unique=True, null=True)
+    youtube = models.URLField(max_length=200, unique=True, null=True)
+    location = models.CharField(max_length=500, default="")
+    rating = models.IntegerField(default=0, validators=[validate_range])
+    favorite = models.BooleanField(default=False)
+    counter = models.IntegerField(default=0)
+    length = models.IntegerField(default=0)
+    offline = models.BooleanField(default=False)
+    genre_type = (
+        ('Fok', 'Folk'),
+        ('Jaz', 'Jazz'),
+        ('Blu', 'Blues'),
+        ('Cls', 'Classical'),
+        ('Roc', 'Rock'),
+        ('Pop', 'Pop'),
+        ('Mel', 'Melody'),
+        ('Hip', 'Hip Hop'),
+        ('Orc', 'Orchestra'),
+        ('Opr', 'Opera'),
+        ('Con', 'Country'),
+        ('Ins', 'Instrumental'),
+        ('Tec', 'Techno'),
+        ('Amb', 'Ambient'),
+        ('HMe', 'Heavy Metal'),
+        ('Dis', 'Disco'),
+        ('EDa', 'Euro Dance'),
+        ('Met', 'Metal'),
+        ('Com', 'Comedy'),
+        ('Mov', 'Movie'),
+        ('Rel', 'Religious'),
+        ('Rim', 'Remix'),
+        ('Und', 'Undefined')
+    )
+    genre = models.CharField(max_length=3, choices=genre_type, default='Und')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class Playlist(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     name = models.CharField(max_length=500, unique=True)
+    track = models.ManyToManyField(Track)
     offline = models.BooleanField(default=False)
     public = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
