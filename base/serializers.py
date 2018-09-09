92a5ced01084f2e086fb1171ae9536cc0ffe9012
Ashutosh Das <git.pyprism@gmail.com>
2018-09-09 17:08:27 +0600

    wip

--- base/serializers.py
+++ base/serializers.py
@@ -1,2 +1,11 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+from .models import Setting, B2Account
+
+
+class Blackbaze(ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+    class Meta:
+        model = B2Account
+        fields = ('',)
+
