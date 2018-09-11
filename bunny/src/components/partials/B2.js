03829cef9b30d22c73f9e7483d5c35121c7d561e
Ashutosh Das <git.pyprism@gmail.com>
2018-09-11 16:23:29 +0600

    added new component

---
+++ bunny/src/components/partials/B2.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default class B2 extends React.Component {
+    render() {
+        return (
+            <div className="card shadow-lg">
+                <h6 className="card-header text-left">
+                    Backblaze B2
+                </h6>
+                <div className="card-body">
+                    <Link to='/music'>
+                        <button type="button" className="btn btn-primary float-left">
+                            Add New Account
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+}
