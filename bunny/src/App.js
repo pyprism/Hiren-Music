d5cdbf5d13141d07a1f8146b1ca5087c7ef7b6fd
Ashutosh Das <git.pyprism@gmail.com>
2018-09-11 12:08:34 +0600

    Added new route

--- bunny/src/App.js
+++ bunny/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
+import Settings from "./components/pages/Settings";
 import Content from './components/layouts/Content';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
                     <Route path="/" exact component={Login} />
                     <Route path="/register" exact component={Register} />
                     <Route path="/music" exact component={Content} />
+                    <Route path="/settings" exact component={Settings} />
                 </div>
             </Router>
         );
