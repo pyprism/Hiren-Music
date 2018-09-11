282c8f8761a71f6a7ec5fca84c855c5fcf9c0d4c
Ashutosh Das <git.pyprism@gmail.com>
2018-09-11 16:23:13 +0600

    refactored

--- bunny/src/components/pages/Settings.js
+++ bunny/src/components/pages/Settings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 import Sidebar from './../layouts/Sidebar';
 import Navbar from './../layouts/Navbar';
+import B2 from '../partials/B2';
 import '../../css/dashboard.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faToolbox, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
@@ -22,17 +23,7 @@ export default class Settings extends React.Component {
                 <Sidebar/>
                 <div id="content">
                     <Navbar/>
-                    <div className="card shadow-lg">
-                        <h6 className="card-header text-left">
-                            Backblaze B2
-                        </h6>
-                        <div className="card-body">
-                            <h5 className="card-title ">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional
-                                content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
+                    <B2/>
                 </div>
             </div>
         )
