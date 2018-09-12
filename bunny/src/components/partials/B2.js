76ef858d4c1b2b0a2276196125a5abc6154cc807
Ashutosh Das <git.pyprism@gmail.com>
2018-09-12 19:47:41 +0600

    small refactor

--- bunny/src/components/partials/B2.js
+++ bunny/src/components/partials/B2.js
@@ -9,7 +9,7 @@ export default class B2 extends React.Component {
                     Backblaze B2
                 </h6>
                 <div className="card-body">
-                    <Link to='/music'>
+                    <Link to='/B2'>
                         <button type="button" className="btn btn-primary float-left">
                             Add New Account
                         </button>
