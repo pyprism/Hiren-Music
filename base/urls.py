ec410b1058f2647884ba48aaa90b73de78c8fa8e
Ashutosh Das <git.pyprism@gmail.com>
2018-09-12 20:29:47 +0600

    added new route

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from base import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('b2', views.BlackbazeModelView)
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('b2/', include(routers)),
 ]
