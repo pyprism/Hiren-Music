b5596dbdc094ecdd561d0ae141a6223a2b0fc885
Ashutosh Das <git.pyprism@gmail.com>
2018-09-13 14:38:28 +0600

    Added new field

--- base/models.py
+++ base/models.py
@@ -45,6 +45,7 @@ class B2Account(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     app_key = models.CharField(max_length=500)
     app_key_id = models.CharField(max_length=500)
+    upload = models.BooleanField(default=True)
     auth_token = models.CharField(max_length=200, null=True)
     auth_token_validity = models.DateTimeField(null=True)
     api_url = models.URLField(null=True)
