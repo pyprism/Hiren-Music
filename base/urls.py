4ad9433b736cb8d49e8346a52871e446099b0599
Ashutosh Das <git.pyprism@gmail.com>
2018-09-13 10:52:04 +0600

    fixed

--- base/urls.py
+++ base/urls.py
@@ -3,10 +3,10 @@ from base import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('b2', views.BlackbazeModelView)
+router.register('blackbaze', views.BlackbazeModelView)
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('b2/', include(routers)),
+    path('b2/', include(router.urls)),
 ]
