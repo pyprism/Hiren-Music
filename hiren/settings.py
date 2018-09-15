0ba149a223d03780d5bc313c0f3c48ef39408863
Ashutosh Das <git.pyprism@gmail.com>
2018-09-15 11:14:03 +0600

    removed pagination

--- hiren/settings.py
+++ hiren/settings.py
@@ -272,8 +272,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES,
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 20
 }
 
 # CORS
