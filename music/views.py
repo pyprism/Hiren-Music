de781fdbbf7c6f949d3d387141c79f72cedfb598
Ashutosh Das <git.pyprism@gmail.com>
2018-09-16 22:19:58 +0600

    added new views

--- music/views.py
+++ music/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .serializers import TrackSerializer, AlbumSerializer, MusicianSerializer, PlaylistSerializer
+from .models import Musician, Track, Album, Playlist
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
-# Create your views here.
+
+class MusicianViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication, BasicAuthentication, SessionAuthentication)
+    serializer_class = MusicianSerializer
+    queryset = Musician.objects.all()
+
+
+class AlbumViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication, BasicAuthentication, SessionAuthentication)
+    serializer_class = AlbumSerializer
+    queryset = Album.objects.all()
+
+
+class TrackViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication, BasicAuthentication, SessionAuthentication)
+    serializer_class = TrackSerializer
+    queryset = Track.objects.all()
+
+
+class PlaylistViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication, BasicAuthentication, SessionAuthentication)
+    serializer_class = PlaylistSerializer
+    queryset = Playlist.objects.all()
+
+
+
