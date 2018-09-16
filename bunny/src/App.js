8ef059f4d9a27c41c4cc669b5f8bc7aa35ba5f91
Ashutosh Das <git.pyprism@gmail.com>
2018-09-16 22:18:33 +0600

    added new route

--- bunny/src/App.js
+++ bunny/src/App.js
@@ -5,6 +5,7 @@ import Register from "./components/pages/Register";
 import Settings from "./components/pages/Settings";
 import Content from './components/layouts/Content';
 import B2Create from './components/pages/B2Create';
+import Upload from './components/pages/Upload';
 
 class App extends Component {
     render() {
@@ -16,6 +17,7 @@ class App extends Component {
                     <Route path="/music" exact component={Content} />
                     <Route path="/settings" exact component={Settings} />
                     <Route path="/B2" exact component={B2Create} />
+                    <Route path="/upload" exact component={Upload} />
                 </div>
             </Router>
         );
