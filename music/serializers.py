66c8c5d17dbac4ed57659585fc7b2198e9b736b6
Ashutosh Das <git.pyprism@gmail.com>
2018-09-17 18:14:37 +0600

    wip

--- music/serializers.py
+++ music/serializers.py
@@ -7,12 +7,12 @@ class MusicianSerializer(ModelSerializer):
     user = serializers.HiddenField(
         default=serializers.CurrentUserDefault(),
     )
-    picture_thumbnail = serializers.ImageField()
+    picture = serializers.ImageField(allow_null=True, write_only=True)
+    picture_thumbnail = serializers.ImageField(read_only=True)
 
     class Meta:
         model = Musician
-        exclude = ('picture', )
-        read_only_fields = ('picture_thumbnail',)
+        fields = '__all__'
 
 
 class AlbumSerializer(ModelSerializer):
@@ -20,17 +20,25 @@ class AlbumSerializer(ModelSerializer):
     user = serializers.HiddenField(
         default=serializers.CurrentUserDefault(),
     )
-    picture_thumbnail = serializers.ImageField()
+    picture = serializers.ImageField(allow_null=True, write_only=True)
+    picture_thumbnail = serializers.ImageField(read_only=True)
 
     class Meta:
         model = Album
-        exclude = ('picture',)
-        read_only_fields = ('picture_thumbnail',)
+        fields = '__all__'
 
     def create(self, validated_data):
-        pass
+        print("Asasass1asa5s4as4asassas24asa4s4a5sas")
+        musician_data = validated_data.pop('musician')
+        musician, created = Musician.objects.get_or_create(user=self.context['request'].user,
+                                                           name=musician_data['name'])
+        print(created)
+        print(musician)
+        album = Album.objects.create(musician=musician,  **validated_data)
+        return album
 
     def update(self, instance, validated_data):
+        print("updatesas5s4asas")
         pass
 
 
