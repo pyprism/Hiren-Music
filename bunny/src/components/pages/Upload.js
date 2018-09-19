58eb463aa54657ae97c12c91429c99a3d4db0afa
Ashutosh Das <git.pyprism@gmail.com>
2018-09-19 21:34:11 +0600

    finished form UI

--- bunny/src/components/pages/Upload.js
+++ bunny/src/components/pages/Upload.js
@@ -3,6 +3,7 @@ import Sidebar from 'components/layouts/Sidebar';
 import Navbar from 'components/layouts/Navbar';
 import Select from 'react-select';
 import sidebarCollapse from 'utils/sidebarCollapse';
+import Creatable from 'react-select/lib/Creatable';
 
 
 export default class Upload extends React.Component {
@@ -12,7 +13,11 @@ export default class Upload extends React.Component {
             loading: true,
             musician: [],
             album: [],
-            selected_genre: null
+            selected_genre: 'Und',
+            selected_musician: '',
+            selected_album: '',
+            title: '',
+            file: null
         };
 
         this.genre_options = [{ label: 'Folk', value: 'Fok'}, {label: 'Jazz', value: 'Jaz'}, {label:'Blues', value:'Blu'}, {label:'Classical', value: 'Cls'}, {label: 'Rock', value:'Roc'}, {label: 'Pop', value:'Pop'},
@@ -49,6 +54,19 @@ export default class Upload extends React.Component {
         });
     }
 
+    albumMusicianBuilder(obj, musician=null, album=null){
+        if(obj.length) {
+            let bunny = [];
+            obj.forEach(function(item) {
+                bunny.push({label: item.name, value: item.name});
+            });
+            if(musician)
+                this.setState({musician: bunny});
+            else
+                this.setState({album: bunny});
+        }
+    }
+
     componentDidMount() {
         if(!localStorage.getItem("token"))
             this.props.history.push("/");
@@ -58,9 +76,8 @@ export default class Upload extends React.Component {
 
         Promise.all([this.getMusician(), this.getAlbum()])
             .then(data =>  {
-                console.log(data[0]);
-                console.log(data[1]);
-                this.setState({musician: data[0], album: data[1]});
+                this.albumMusicianBuilder(data[0], true);
+                this.albumMusicianBuilder(data[1], false, true);
                 this.setState({loading: false});
             })
             .catch(err => {
@@ -68,14 +85,39 @@ export default class Upload extends React.Component {
             })
     }
 
-    handleGenreChange = (selected_genre) => {
+    handleGenreChange(selected_genre) {
         this.setState({ selected_genre });
         console.log(selected_genre['value']);
+    };
+
+    handleAlbumChange(selected_album) {
+        this.setState({ selected_album });
+    }
+
+    handleMusicianChange(selected_musician){
+        this.setState({ selected_musician });
+    }
+
+    handleTitleChange(event){
+        this.setState({title: event.target.value});
+    }
+
+    handleFileChange(event) {
+        this.setState({file: event.target.files[0]});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        if(!this.state.file || this.state.title.length === 0) {
+            return;
+        }
+        let formData = new FormData();
+        formData.append('title', this.state.title);
     }
 
     render() {
 
-        const { musician, album, selected_genre } = this.state;
+        const { musician, album, selected_genre, title, file } = this.state;
 
         if(this.state.loading) {
             return (
@@ -111,22 +153,43 @@ export default class Upload extends React.Component {
                             Upload New Song
                         </h6>
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={this.handleSubmit.bind(this)}>
                                 <div className="form-group">
                                     <label htmlFor="exampleInputEmail1">Title</label>
-                                    <input type="text" className="form-control" id="exampleInputEmail1"
+                                    <input type="text" value={title} onChange={this.handleTitleChange.bind(this)} className="form-control" id="exampleInputEmail1"
                                            placeholder="Enter title"/>
                                 </div>
                                 <div className="form-group">
                                     <label >Genre</label>
                                     <Select
+                                        isClearable
                                         value={selected_genre}
                                         onChange={this.handleGenreChange}
                                         options={this.genre_options}
-                                        isSearchable={true}
+                                        isSearchable
                                     />
                                 </div>
-                                <button type="submit" className="btn btn-primary">Submit</button>
+                                <div className="form-group">
+                                    <label >Album</label>
+                                    <Creatable
+                                        isClearable
+                                        onChange={this.handleAlbumChange}
+                                        options={album}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label >Musician</label>
+                                    <Creatable
+                                        isClearable
+                                        onChange={this.handleMusicianChange}
+                                        options={musician}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label >Music File</label>
+                                    <input type="file" onChange={this.handleFileChange.bind(this)} className="form-control-file" accept="audio/*" />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Upload</button>
                             </form>
                         </div>
                     </div>
