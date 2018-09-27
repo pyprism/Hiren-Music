3277d9f29ca672155905744ad19401d557bd45e9
Ashutosh Das <git.pyprism@gmail.com>
2018-09-27 22:02:04 +0600

    changed component's name

--- bunny/src/App.js
+++ bunny/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Login from "components/pages/Login";
 import Register from "components/pages/Register";
 import Settings from "components/pages/Settings";
-import Content from 'components/layouts/Content';
+import Tracks from 'components/pages/Tracks';
 import B2Create from 'components/pages/B2Create';
 import Upload from 'components/pages/Upload';
 
@@ -14,7 +14,7 @@ class App extends Component {
                 <div>
                     <Route path="/" exact component={Login} />
                     <Route path="/register" exact component={Register} />
-                    <Route path="/music" exact component={Content} />
+                    <Route path="/tracks" exact component={Tracks} />
                     <Route path="/settings" exact component={Settings} />
                     <Route path="/B2" exact component={B2Create} />
                     <Route path="/upload" exact component={Upload} />
