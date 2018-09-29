570ed76f707462ca1446ea2c64cdc9fb5973f249
Ashutosh Das <git.pyprism@gmail.com>
2018-09-29 14:12:42 +0600

    added storage view

--- base/views.py
+++ base/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import views
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authtoken.models import Token
-from .serializers import BlackbazeSerializer, SettingsSerializer
+from .serializers import BlackbazeSerializer, SettingsSerializer, StorageSerializer
 from django.http import JsonResponse
 from rest_framework import status
 from django.contrib import auth
 from base.models import Account, B2Account, Setting
 from django.views.decorators.csrf import csrf_exempt
 from utils.b2 import b2_auth
+from utils.storage import check_storage
 
 
 @csrf_exempt
@@ -50,6 +49,9 @@ def register(request):
 
 
 class BlackbazeModelView(ModelViewSet):
+    """
+    Blackbaze B2 account management
+    """
     authentication_classes = (TokenAuthentication, BasicAuthentication, SessionAuthentication)
     serializer_class = BlackbazeSerializer
     queryset = B2Account.objects.all()
@@ -77,5 +79,15 @@ class SettingsModelView(ModelViewSet):
     queryset = Setting.objects.all()
 
 
+class StorageView(views.APIView):
+    """
+    Return local storage status
+    """
+    authentication_classes = (TokenAuthentication, BasicAuthentication, SessionAuthentication)
+
+    def get(self, request):
+        data = [check_storage()]
+        results = StorageSerializer(data, many=True).data
+        return Response(results)
 
 
