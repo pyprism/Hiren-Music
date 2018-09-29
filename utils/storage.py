4ba500cc841f4ce6a168705357048c065e3a892f
Ashutosh Das <git.pyprism@gmail.com>
2018-09-29 22:11:48 +0600

    Added percentage

--- utils/storage.py
+++ utils/storage.py
@@ -15,18 +15,21 @@ def check_storage():
     :return:
     """
     total, used, free = shutil.disk_usage(BASE_DIR)
+    percentage = (used / total) * 100
     base = {'total': filesizeformat(total), 'used': filesizeformat(used),
-            'free': filesizeformat(free)}
+            'free': filesizeformat(free), 'percentage': round(percentage)}
     try:
         up_total, up_used, up_free = shutil.disk_usage(BASE_DIR + '/media/music/upload/')
+        percentage = (used / total) * 100
         upload = {'total': filesizeformat(up_total), 'used': filesizeformat(up_used),
-                  'free': filesizeformat(up_free)}
+                  'free': filesizeformat(up_free), 'percentage': round(percentage)}
     except FileNotFoundError:
         upload = 'not found'
     try:
         down_total, down_used, down_free = shutil.disk_usage(BASE_DIR + '/media/music/download/')
+        percentage = (used / total) * 100
         download = {'total': filesizeformat(down_total), 'used': filesizeformat(down_used),
-                    'free': filesizeformat(down_free)}
+                    'free': filesizeformat(down_free), 'percentage': round(percentage)}
     except FileNotFoundError:
         download = 'not found'
     return {'upload': upload, 'download': download, 'base': base}
