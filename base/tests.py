aa007b4ec3aab2ac92b2eacff9372808fb32e1cd
Ashutosh Das <git.pyprism@gmail.com>
2018-09-30 12:19:26 +0600

    added new test

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+from django.shortcuts import reverse
+from rest_framework.test import APIRequestFactory, APIClient
+from .models import Account
+from django.utils import timezone
+import json
 
-# Create your tests here.
+
+class Login(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        Account.objects.create_user('hiren', 'password')
+
+    def test_auth_token(self):
+        response = self.client.post(reverse("login"), {'username': 'hiren', 'password': 'password'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('token' in response.json())
+
+    def test_error(self):
+        response = self.client.post(reverse("login"), {'username': 'hiren', 'password': 'bad password'})
+        self.assertEqual(response.status_code, 401)
+        self.assertTrue('error' in response.json())
+
