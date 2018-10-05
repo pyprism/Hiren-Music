7e071bb2d5f5cc2689927133f07d6bcb96c17376
Ashutosh Das <git.pyprism@gmail.com>
2018-10-05 18:20:50 +0600

    changed icons

--- bunny/src/components/partials/Player.js
+++ bunny/src/components/partials/Player.js
@@ -1,40 +1,69 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPlayCircle, faStepBackward, faStepForward} from "@fortawesome/free-solid-svg-icons";
+import {faVolumeDown, faVolumeUp, faRandom} from "@fortawesome/free-solid-svg-icons";
 
 
 export default class Player extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            playing: false
+        };
+    }
 
     componentDidMount(){
         console.log("asa");
     }
 
+    playButton() {
+        if(this.state.playing)
+            return (
+                <FontAwesomeIcon icon={faPlayCircle} size="2x" spin/>
+            )
+        return (
+            <FontAwesomeIcon icon={faPlayCircle} size="2x"/>
+        )
+    }
 
     render() {
         return (
             <nav className="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark" style={{marginBottom: '0px'}}>
-                <a className="navbar-brand" href="#">Bottom navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
-                        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"/>
-                </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    <ul className="navbar-nav mr-auto mx-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="javascript:;">
+                                <FontAwesomeIcon icon={faStepBackward} size="2x"/>
+                            </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <a className="nav-link" href="#">
+                                {this.playButton()}
+                            </a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="#">Disabled</a>
+                            <a className="nav-link" href="#">
+                                <FontAwesomeIcon icon={faStepForward} size="2x"/>
+                            </a>
                         </li>
-                        <li className="nav-item dropup">
-                            <a className="nav-link dropdown-toggle" href="https://getbootstrap.com" id="dropdown10"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropup</a>
-                            <div className="dropdown-menu" aria-labelledby="dropdown10">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                            </div>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <FontAwesomeIcon icon={faRandom} size="2x"/>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <FontAwesomeIcon icon={faVolumeDown} size="2x"/>
+                            </a>
+                        </li>
+                        <li className="nav-item" style={{'marginTop': '12px'}}>
+                            <input type="range" min="0" max="100" step="1"/>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                <FontAwesomeIcon icon={faVolumeUp} size="2x"/>
+                            </a>
                         </li>
                     </ul>
                 </div>
